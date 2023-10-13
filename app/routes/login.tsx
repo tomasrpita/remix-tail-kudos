61dbc9a4bae3964f1905b9b11c59e7cb161cc0c4
Tomas R. Pita <tomasrpita@gmail.com>
2023-10-13 12:39:45 +0200

    Fix change form button on login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -10,63 +10,63 @@ import { validateEmail, validateName, validatePassword } from '~/utils/validator
 import { login, register, getUser } from '~/utils/auth.server'
 
 export const loader: LoaderFunction = async ({ request }) => {
-    // If there's already a user in the session, redirect to the home page
-    return await getUser(request) ? redirect('/') : null
+  // If there's already a user in the session, redirect to the home page
+  return await getUser(request) ? redirect('/') : null
 }
 
 export const action: ActionFunction = async ({ request }) => {
-    const form = await request.formData();
-    const action = form.get("_action");
-    const email = form.get("email");
-    const password = form.get("password");
-    let firstName = form.get("firstName");
-    let lastName = form.get("lastName");
+  const form = await request.formData();
+  const action = form.get("_action");
+  const email = form.get("email");
+  const password = form.get("password");
+  let firstName = form.get("firstName");
+  let lastName = form.get("lastName");
 
-    // If not all data was passed, error
-    if (
-        typeof action !== "string" ||
-        typeof email !== "string" ||
-        typeof password !== "string"
-    ) {
-        return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+  // If not all data was passed, error
+  if (
+    typeof action !== "string" ||
+    typeof email !== "string" ||
+    typeof password !== "string"
+  ) {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+  }
+
+  // If not all data was passed, error
+  if (
+    action === 'register' && (
+      typeof firstName !== "string" ||
+      typeof lastName !== "string"
+    )
+  ) {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+  }
+
+  // Validate email & password
+  const errors = {
+    email: validateEmail(email),
+    password: validatePassword(password),
+    ...(action === 'register' ? {
+      firstName: validateName(firstName as string || ''),
+      lastName: validateName(lastName as string || ''),
+    } : {})
+  };
+
+  //  If there were any errors, return them
+  if (Object.values(errors).some(Boolean))
+    return json({ errors, fields: { email, password, firstName, lastName }, form: action }, { status: 400 });
+
+  switch (action) {
+    case 'login': {
+      return await login({ email, password })
     }
-
-    // If not all data was passed, error
-    if (
-        action === 'register' && (
-            typeof firstName !== "string" ||
-            typeof lastName !== "string"
-        )
-    ) {
-        return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+    case 'register': {
+      firstName = firstName as string
+      lastName = lastName as string
+      return await register({ email, password, firstName, lastName })
     }
-
-    // Validate email & password
-    const errors = {
-        email: validateEmail(email),
-        password: validatePassword(password),
-        ...(action === 'register' ? {
-            firstName: validateName(firstName as string || ''),
-            lastName: validateName(lastName as string || ''),
-        } : {})
-    };
-
-    //  If there were any errors, return them
-    if (Object.values(errors).some(Boolean))
-        return json({ errors, fields: { email, password, firstName, lastName }, form: action }, { status: 400 });
-
-    switch (action) {
-        case 'login': {
-            return await login({ email, password })
-        }
-        case 'register': {
-            firstName = firstName as string
-            lastName = lastName as string
-            return await register({ email, password, firstName, lastName })
-        }
-        default:
-            return json({ error: `Invalid Form Data` }, { status: 400 });
-    }
+    default:
+      return json({ error: `Invalid Form Data` }, { status: 400 });
+  }
 }
 
 export default function Login() {
@@ -83,7 +83,6 @@ export default function Login() {
     lastName: actionData?.fields?.firstName || '',
   })
 
-  https://github.com/sabinadams/kudos-remix-mongodb-prisma/blob/part-2/app/routes/login.tsx
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, field: string) => {
     setFormData(form => ({ ...form, [field]: event.target.value }))
   }
@@ -91,35 +90,35 @@ export default function Login() {
   useEffect(() => {
     // Clear the form if we switch forms
     if (!firstLoad.current) {
-        const newState = {
-            email: '',
-            password: '',
-            firstName: '',
-            lastName: ''
-        }
-        setErrors(newState)
-        setFormError('')
-        setFormData(newState)
+      const newState = {
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: ''
+      }
+      setErrors(newState)
+      setFormError('')
+      setFormData(newState)
     }
-}, [action])
+  }, [action])
 
-useEffect(() => {
+  useEffect(() => {
     if (!firstLoad.current) {
-        setFormError('')
+      setFormError('')
     }
-}, [formData])
+  }, [formData])
 
-useEffect(() => {
+  useEffect(() => {
     // We don't want to reset errors on page load because we want to see them
     firstLoad.current = false
-}, [])
+  }, [])
 
 
   return (
     <Layout>
       <div className="h-full justify-center items-center flex flex-col gap-y-4">
         <button
-          onClick={() => setAction(action == 'login' ? 'register' : 'login')}
+          onClick={() => setAction(prevAction => prevAction === 'login' ? 'register' : 'login')}
           className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
         >
           {action === 'login' ? 'Sign Up' : 'Sign In'}
@@ -129,7 +128,7 @@ useEffect(() => {
           {action === 'login' ? 'Log In To Give Some Praise!' : 'Sign Up To Get Started!'}
         </p>
         <form method="post" className="rounded-2xl bg-gray-200 p-6 w-96">
-        <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">{formError}</div>
+          <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">{formError}</div>
           <FormField
             htmlFor="email"
             label="Email"
@@ -166,7 +165,9 @@ useEffect(() => {
           )}
 
           <div className="w-full text-center">
-            <button type="submit" name="_action" value={action} className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+            <button 
+              type="submit" name="_action" value={action} 
+              className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
               {
                 action === 'login' ? "Sign In" : "Sign Up"
               }
